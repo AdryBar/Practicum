1076656e6b89b592754e872446a968143443ced8
AndryBar <shondi-7@mail.ru>
2023-03-30 21:10:19 +0300

    В рот чих пых

--- .idea/src/Pra.java
+++ .idea/src/Pra.java
@@ -1,67 +1,26 @@
-import java.util.Scanner;
+import java.util.Random;
 
-class Pra {
+class Praktikum {
     public static void main(String[] args) {
-        double rateUSD = 78, rateEUR = 85, rateJPY = 0.74;
+        // Объявите пустой массив трат за неделю (7 дней)
+        int[] expenses = new int[7];
 
-        Scanner scanner = new Scanner(System.in);
+        Random random = new Random(); // Генерирует случайное число
 
-        System.out.println("Сколько денег у вас осталось до зарплаты?");
-        double moneyBeforeSalary = scanner.nextDouble();
-
-        System.out.println("Сколько дней до зарплаты?");
-        int daysBeforeSalary = scanner.nextInt();
-
-        while (true) { //настройте бесконечный цикл здесь
-            System.out.println("Что вы хотите сделать? ");
-        System.out.println("1 - Конвертировать валюту");
-        System.out.println("2 - Получить совет");
-        System.out.println("0 - Выход"); // Новый пункт меню, осталось только реализовать логику
-
-        int command = scanner.nextInt();
-
-        if (command == 1) {
-            System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - USD, 2 - EUR или 3 - JPY.");
-            int currency = scanner.nextInt();
-
-            if (currency == 1) {
-                System.out.println("Ваши сбережения в долларах: " + moneyBeforeSalary / rateUSD);
-            } else if (currency == 2) {
-                System.out.println("Ваши сбережения в евро: " + moneyBeforeSalary / rateEUR);
-            } else if (currency == 3) {
-                System.out.println("Ваши сбережения в йенах: " + moneyBeforeSalary / rateJPY);
-            } else {
-                System.out.println("Валюта не поддерживается.");
-            }
-
-        } else if (command == 2) {
-            if (moneyBeforeSalary < 3000) {
-                System.out.println("Сегодня лучше поесть дома. Экономьте и вы дотянете до зарплаты!");
-            } else if (moneyBeforeSalary < 10000) {
-                if (daysBeforeSalary < 10) {
-                    System.out.println("Окей, пора в Макдак!");
-                } else {
-                    System.out.println("Сегодня лучше поесть дома. Экономьте и вы дотянете до зарплаты!");
-                }
-            } else if (moneyBeforeSalary < 30000) {
-                if (daysBeforeSalary < 10) {
-                    System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место. :)");
-                } else {
-                    System.out.println("Окей, пора в Макдак!");
-                }
-            } else {
-                if (daysBeforeSalary < 10) {
-                    System.out.println("Отлично! Заказывайте крабов!");
-                } else {
-                    System.out.println("Неплохо! Прикупите долларов и зайдите поужинать в классное место. :)");
-                }
-            }
-        } else if (command == 0) {
-            System.out.println("Выход");
-            break;
-        } else {
-            System.out.println("Извините, такой команды пока нет.");
-            }
+        // Допишите условие цикла for, чтобы заполнить массив случайными тратами
+        for (int i = 0; i <= expenses.length; i++) {
+            expenses[i] = random.nextInt(1000);
         }
+
+        System.out.println("Траты за неделю:");
+        // Выведите с помощью цикла все траты за неделю в виде: "День ... . Потрачено рублей: ..."
+        System.out.println("День " + (i + 1) + " . Потрачено рублей: " + expenses[i] );
+
+        int sum = 0;
+        // Посчитайте и выведите сумму трат за неделю — используйте цикл и здесь
+        sum = sum +  expenses[i];
+
+        System.out.println("Траты в рублях за неделю: " + sum);
     }
 }
+
